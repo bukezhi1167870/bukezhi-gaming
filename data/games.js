0aeed73957859e64e00dd0cdb99c378f0e059b6c
bukezhi1167870 <3851736037@qq.com>
2026-09-18 17:55:00 +0800

    Update games.js

--- data/games.js
+++ data/games.js
@@ -27,7 +27,7 @@ window.games = [
     name: "沉溺之下",
     url: "games/BDSM Down/index.html",
     image: "images/game-D01.png",
-    tags: ["单人", "调教", "休闲", "骰子", "轻策"],
+    tags: ["单人", "运气", "休闲", "骰子", "轻策"],
     desc: "在一段段BDSM关系的深入下，你能否把控边界？",
     category: "single"
   },
